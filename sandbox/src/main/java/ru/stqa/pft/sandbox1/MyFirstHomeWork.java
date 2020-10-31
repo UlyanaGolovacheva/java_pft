d765bbe17f9aff2a1b505f0cd195e343a205475c
Ульяна Головачева <73671100+UlyanaGolovacheva@users.noreply.github.com>
2020-10-31 21:28:55 +0300

    Вычисление расстояния м/у точками при помощи метода в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox1/MyFirstHomeWork.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox1/MyFirstHomeWork.java
@@ -3,14 +3,8 @@ package ru.stqa.pft.sandbox1;
 public class MyFirstHomeWork {
   public static void main(String[] args) {
 
-    Point p1 = new Point(4, 6);
-    Point p2 = new Point(10, 16);
-    System.out.println("Расстояние между точками " + "А(" + p1.x + ";" + p1.y + ")" + " и " +
-            "В(" + p2.x + ";" + p2.y + ")" + " = " + BetweenPoints(p1, p2));
-  }
-
-  public static double BetweenPoints(Point p1, Point p2) {
-    return Math.sqrt(((p2.x - p1.x) * (p2.x - p1.x)) + ((p2.y - p1.y) * (p2.y - p1.y)));
-
+    Point z = new Point(-1,-1,12,3);
+        System.out.println("Расстояние между точками " + "А(" + z.x1 + ";" + z.y1 + ")" + " и " +
+            "В(" + z.x2 + ";" + z.y2 + ")" + " = " + z.BetweenPoints());
   }
 }
