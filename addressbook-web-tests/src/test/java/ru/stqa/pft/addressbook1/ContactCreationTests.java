ea4794dde5c1095c85aa9e0538aba462883fe148
Ульяна Головачева <73671100+UlyanaGolovacheva@users.noreply.github.com>
2020-11-05 17:29:26 +0300

    Test new Contact Refactoring

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook1/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook1/ContactCreationTests.java
@@ -1,9 +1,7 @@
 package ru.stqa.pft.addressbook1;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
@@ -16,53 +14,75 @@ public class ContactCreationTests {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/edit.php");
+    login("admin", "secret");
+  }
+
+  private void login(String login, String password) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(login);
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @Test
   public void testContactCreation() throws Exception {
+    initContactPage();
+    fillContactForm(new ContactData("Ulyana", "Aleksandrovna", "Golovacheva", "Ulita", "QA", "T2",
+                    "theatralniy,60\nRostov-na-Donu", "89081903221", "ulyana.golovacheva@tele2.ru", "9", "October", "test1"));
+    submitContactCreation();
+    returnToHomePage();
+  //  wd.findElement(By.linkText("Logout")).click();
+  //  wd.findElement(By.name("user")).clear();
+  //  wd.findElement(By.name("user")).sendKeys("admin");
+  }
+
+  private void returnToHomePage() {
+    wd.findElement(By.linkText("home")).click();
+  }
+
+  private void submitContactCreation() {
+    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  private void fillContactForm(ContactData contactData) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Ulyana");
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getName());
     wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys("Aleksandrovna");
+    wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddlname());
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Golovacheva");
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastname());
     wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys("Ulita");
+    wd.findElement(By.name("nickname")).sendKeys(contactData.getNickname());
     wd.findElement(By.name("title")).clear();
-    wd.findElement(By.name("title")).sendKeys("QA");
+    wd.findElement(By.name("title")).sendKeys(contactData.getTitle());
     wd.findElement(By.name("company")).click();
     wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys("T2");
+    wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
     wd.findElement(By.name("address")).click();
     wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys("theatralniy,60\nRostov-na-Donu");
+    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
     wd.findElement(By.name("mobile")).click();
     wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys("89081903221");
+    wd.findElement(By.name("mobile")).sendKeys(contactData.getMobile());
     wd.findElement(By.name("email")).click();
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("ulyana.golovacheva@tele2.ru");
+    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
     wd.findElement(By.name("bday")).click();
-    new Select(wd.findElement(By.name("bday"))).selectByVisibleText("9");
+    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getBday());
     wd.findElement(By.xpath("//option[@value='9']")).click();
     wd.findElement(By.name("bmonth")).click();
-    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText("October");
+    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getBmonth());
     wd.findElement(By.xpath("//option[@value='October']")).click();
     wd.findElement(By.name("new_group")).click();
-    new Select(wd.findElement(By.name("new_group"))).selectByVisibleText("test1");
+    new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
     wd.findElement(By.xpath("(//option[@value='3'])[3]")).click();
-    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
-    wd.findElement(By.linkText("home")).click();
-    wd.findElement(By.linkText("Logout")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+  }
+
+  private void initContactPage() {
+    wd.findElement(By.linkText("add new")).click();
   }
 
   @AfterMethod(alwaysRun = true)
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook1/ContactData.java
@@ -0,0 +1,79 @@
+package ru.stqa.pft.addressbook1;
+
+public class ContactData {
+  private final String name;
+  private final String middlname;
+  private final String lastname;
+  private final String nickname;
+  private final String title;
+  private final String company;
+  private final String address;
+  private final String mobile;
+  private final String email;
+  private final String bday;
+  private final String bmonth;
+  private final String group;
+
+  public ContactData(String name, String middlname, String lastname, String nickname, String title, String company, String address, String mobile, String email, String bday, String bmonth, String group) {
+    this.name = name;
+    this.middlname = middlname;
+    this.lastname = lastname;
+    this.nickname = nickname;
+    this.title = title;
+    this.company = company;
+    this.address = address;
+    this.mobile = mobile;
+    this.email = email;
+    this.bday = bday;
+    this.bmonth = bmonth;
+    this.group = group;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getMiddlname() {
+    return middlname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getBday() {
+    return bday;
+  }
+
+  public String getBmonth() {
+    return bmonth;
+  }
+
+  public String getGroup() {
+    return group;
+  }
+}
