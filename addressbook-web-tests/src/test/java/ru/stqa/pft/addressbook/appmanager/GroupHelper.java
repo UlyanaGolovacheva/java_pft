601387dae28b27021b5bfa184703793bed11b3c5
Ульяна Головачева <73671100+UlyanaGolovacheva@users.noreply.github.com>
2020-11-10 23:15:38 +0300

    Task5_double2.  +ContactHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -30,35 +30,6 @@ public class GroupHelper extends HelperBase {
     click(By.name("new"));
   }
 
-  public void returnToHomePage() {
-    click(By.linkText("home"));
-  }
-
-  public void submitContactCreation() {
-    click(By.xpath("(//input[@name='submit'])[2]"));
-  }
-
-  public void fillContactForm(ContactData contactData) {
-    type(By.name("firstname"), contactData.getName());
-    type(By.name("middlename"), contactData.getMiddlname());
-    type(By.name("lastname"), contactData.getLastname());
-    type(By.name("nickname"), contactData.getNickname());
-    type(By.name("title"), contactData.getTitle());
-    type(By.name("company"), contactData.getCompany());
-    type(By.name("address"), contactData.getAddress());
-    type(By.name("mobile"), contactData.getMobile());
-    type(By.name("email"), contactData.getEmail());
-    click(By.name("bday"));
-    select(By.name("bday"), contactData.getBday());
-    click(By.xpath("//option[@value='9']"));
-    click(By.name("bmonth"));
-    select(By.name("bmonth"), contactData.getBmonth());
-    click(By.xpath("//option[@value='October']"));
-    click(By.name("new_group"));
-    select(By.name("new_group"), contactData.getGroup());
-    click(By.xpath("//select[5]/option[2]"));
-  }
-
   public void deleteSelectedGroups() {
     click(By.name("delete"));
   }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -8,9 +8,9 @@ public class ContactCreationTests extends TestBase {
   @Test
   public void testContactCreation() throws Exception {
     app.getNavigationHelper().initContactPage();
-    app.getGroupHelper().fillContactForm(new ContactData("Ulyana", "Aleksandrovna", "Golovacheva", "Ulita", "QA", "T2",
+    app.getContactHelper().fillContactForm(new ContactData("Ulyana", "Aleksandrovna", "Golovacheva", "Ulita", "QA", "T2",
                     "theatralniy,60\nRostov-na-Donu", "89081903221", "ulyana.golovacheva@tele2.ru", "9", "October", "test1"));
-    app.getGroupHelper().submitContactCreation();
-    app.getGroupHelper().returnToHomePage();
+    app.getContactHelper().submitContactCreation();
+    app.getContactHelper().returnToHomePage();
   }
 }
